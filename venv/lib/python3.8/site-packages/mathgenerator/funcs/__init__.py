242613c5b6f129bbe0d42c099083fe1f39cabbc3
Krmogi <krishayras@gmail.com>
2022-10-23 06:30:16 -0700

    venv

---
+++ venv/lib/python3.8/site-packages/mathgenerator/funcs/__init__.py
@@ -0,0 +1,13 @@
+import random
+import math
+import fractions
+
+from ..__init__ import *
+
+from .algebra import *
+from .basic_math import *
+from .calculus import *
+from .computer_science import *
+from .geometry import *
+from .misc import *
+from .statistics import *
